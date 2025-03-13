1246a294ed285c731f0eae07bb99719329090463
Michelle Tolentino <misheruvynx@gmail.com>
2025-03-13 19:32:44 +0800

    binary search fix

--- src/main/java/com/example/CodingPractice/medium/BinarySearch.java
+++ src/main/java/com/example/CodingPractice/medium/BinarySearch.java
@@ -1,11 +1,13 @@
 package com.example.CodingPractice.medium;
 
+// binary search: return -1 if not found, index if found
+
 public class BinarySearch {
     public static int binarySearch(int[] arr, int target){
         int low = 0, high = arr.length - 1;
         while(low <= high) {
             int mid = low + (high - low) / 2;
-            if(arr[mid] == mid) {
+            if(arr[mid] == target) {
                 return mid;
             } else if (arr[mid] < target) {
                 low = mid + 1; // search right
@@ -17,6 +19,6 @@ public class BinarySearch {
     }
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5,6,7,8,9,10};
-
+        System.out.println(binarySearch(arr, 5));
     }
 }

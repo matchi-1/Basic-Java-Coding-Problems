2705865eeccd344f01940cc2664b5677d73e88f1
Michelle Tolentino <misheruvynx@gmail.com>
2025-03-26 18:36:38 +0800

    hIndex - leetcode medium

---
+++ src/main/java/com/example/CodingPractice/medium/HIndex.java
@@ -0,0 +1,68 @@
+package com.example.CodingPractice.medium;
+
+import java.util.Arrays;
+
+/*
+Given an array of integers citations where citations[i] is the number of citations a researcher received for their ith paper, return the researcher's h-index.
+
+According to the definition of h-index on Wikipedia: The h-index is defined as the maximum value of h such that the given researcher has published at least h papers that have each been cited at least h times.
+
+Example 1:
+
+Input: citations = [3,0,6,1,5]
+Output: 3
+Explanation: [3,0,6,1,5] means the researcher has 5 papers in total and each of them had received 3, 0, 6, 1, 5 citations respectively.
+Since the researcher has 3 papers with at least 3 citations each and the remaining two with no more than 3 citations each, their h-index is 3.
+Example 2:
+
+Input: citations = [1,3,1]
+Output: 1
+
+
+Constraints:
+
+n == citations.length
+1 <= n <= 5000
+0 <= citations[i] <= 1000
+*/
+public class HIndex {
+
+    public int hIndex(int[] citations) {
+        int n = citations.length;
+        if (n == 0 || (n == 1 && citations[0] == 0))
+            return 0;
+        else if (n == 1)
+            return 1;
+
+        Arrays.sort(citations);
+        for (int i = 0; i < n; i++) {
+            if (citations[i] >= n - i) {
+                return n - i;
+            }
+        }
+        return 0;
+    }
+
+    public int hIndex2(int[] citations) {   /*  O(n) solution from leetcode  */
+        int n = citations.length;
+        int[] buckets = new int[n+1];
+        for(int c : citations) {
+            if(c >= n) {
+                buckets[n]++;
+            } else {
+                buckets[c]++;
+            }
+        }
+        int count = 0;
+        for(int i = n; i >= 0; i--) {
+            count += buckets[i];
+            if(count >= i) {
+                return i;
+            }
+        }
+        return 0;
+    }
+
+
+
+}
